2d6c16b11f11265555dbf1f3821f40b2e539db52
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-09-10 13:48:48 -0500

    implementa repository

---
+++ src/main/java/com/felipelopez/Biblioteca/repository/FechaPublicacionRepository.java
@@ -0,0 +1,7 @@
+package com.felipelopez.Biblioteca.repository;
+
+import com.felipelopez.Biblioteca.model.entity.FechaPublicacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FechaPublicacionRepository extends JpaRepository<FechaPublicacion, Long> {
+}

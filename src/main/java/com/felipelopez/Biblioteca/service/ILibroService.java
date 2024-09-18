9a73eee4d2b5a22e157e52e2c94c3c6682f4c4c4
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-09-18 15:54:46 -0500

    implementa service

---
+++ src/main/java/com/felipelopez/Biblioteca/service/ILibroService.java
@@ -0,0 +1,19 @@
+package com.felipelopez.Biblioteca.service;
+
+import com.felipelopez.Biblioteca.entity.Libro;
+
+import java.util.List;
+
+public interface ILibroService {
+
+    public List<Libro> obtenerTodosLosLibros();
+
+    public Libro crearLibro(Libro libro);
+
+    public Libro obtenerLibroPorId(Long idLibro);
+
+    void eliminarLibro(Long idLibro);
+
+    public Libro actualizarLibro(Long idLibro, Libro libroActualizado);
+
+}

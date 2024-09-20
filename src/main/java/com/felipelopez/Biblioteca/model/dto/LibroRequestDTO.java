e79b5137b64ac0b0195aa94a5a87458bd54b1331
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-09-19 22:38:19 -0500

    fix databases

--- src/main/java/com/felipelopez/Biblioteca/model/dto/LibroRequestDTO.java
+++ src/main/java/com/felipelopez/Biblioteca/model/dto/LibroRequestDTO.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 @Data
 public class LibroRequestDTO {
 
+    public Long idLibro;
     @NotBlank(message = "El nombre del Libro no puede estar vacio")
     private String tituloLibro;
     @NotBlank(message = "El nombre del Autor no puede estar vacio")
